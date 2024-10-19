f6c3f6558ee4625cad4c1ae03b0db0e173306779
Aliguliev <ealiguliev@gmail.com>
2024-10-19 09:11:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const allSongs=[
         title: "Scratching The Surface",
         artist: "Quincy Larson",
         duration: "4:25",
-        src: "https://cdn.freecodecamp.org/curriculum/js-music-player/scratching-the-surface.mp3",
+        src: "./scratching-the-surface.mp3",
     },
     {
         
